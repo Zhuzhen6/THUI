3cafa1ff66e72b054a2a4450753980de35077d50
ZhuZhen <295111806@qq.com>
2018-11-07 15:54:22 +0800

    aa

---
+++ THUIDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  THUIDemo
+//
+//  Created by TonghuiMac on 2018/11/7.
+//  Copyright © 2018 TonghuiMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ THUIDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  THUIDemo
+//
+//  Created by TonghuiMac on 2018/11/7.
+//  Copyright © 2018 TonghuiMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
