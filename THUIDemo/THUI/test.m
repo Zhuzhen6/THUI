162cf2c8be7aac973a0c3687e23eeeeaf7c38e4e
ZhuZhen <295111806@qq.com>
2018-11-07 15:55:38 +0800

    test

---
+++ THUIDemo/THUI/test.m
@@ -0,0 +1,21 @@
+//
+//  test.m
+//  THUIDemo
+//
+//  Created by TonghuiMac on 2018/11/7.
+//  Copyright © 2018 TonghuiMac. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
